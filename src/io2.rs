79063da6802414a099993dce3778a2f5dac4ccc2
Faust Fu <faust.fu@gmail.com>
2020-02-05 16:11:34 +0000

    Dev: read real file in io2.

--- src/io2.rs
+++ src/io2.rs
@@ -1,4 +1,5 @@
 use std::env; //use std::env::args_os to process invalid unicode strings.
+use std::fs;
 
 pub fn io2() {
   let args: Vec<String> = env::args().collect();
@@ -8,4 +9,7 @@ pub fn io2() {
 
   println!("Searching for {}", query);
   println!("In file {}", filename);
+  let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+  println!("With text:\n{}", contents);
 }
