8e483094ba78d73c5135538009d53220666edc36
Faust Fu <faust.fu@gmail.com>
2020-02-10 16:10:14 +0000

    Dev: use a pure function to parse arguments.

--- src/io2.rs
+++ src/io2.rs
@@ -4,12 +4,19 @@ use std::fs;
 pub fn io2() {
   let args: Vec<String> = env::args().collect();
 
-  let query = &args[1];
-  let filename = &args[2];
+  let (query, filename) = parse_config(&args);
 
   println!("Searching for {}", query);
   println!("In file {}", filename);
   let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
   println!("With text:\n{}", contents);
+}
+
+// The pure function holds the logic that determines which argument goes in which variable and passes the values back to main.
+fn parse_config(args: &[String]) -> (&str, &str) {
+  let query = &args[1];
+  let filename = &args[2];
+
+  (query, filename)
 }
