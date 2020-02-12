1462d9e242c9d67ec68371d1ae3f2918b6c09b9f
Faust Fu <faust.fu@gmail.com>
2020-02-12 16:11:11 +0000

    Dev: implement new method to encapsulate initial logic.

--- src/io2.rs
+++ src/io2.rs
@@ -4,7 +4,7 @@ use std::fs;
 pub fn io2() {
   let args: Vec<String> = env::args().collect();
 
-  let config = parse_config(&args);
+  let config = Config::new(&args);
 
   println!("Searching for {}", config.query);
   println!("In file {}", config.filename);
@@ -21,10 +21,12 @@ struct Config {
   filename: String,
 }
 
-// The function holds the logic that determines which argument goes in which variable and passes the values back to main.
-fn parse_config(args: &[String]) -> Config {
-  let query = args[1].clone();
-  let filename = args[2].clone();
+// Encapsulate the initial logic of members into a method likes OOP.
+impl Config {
+  fn new(args: &[String]) -> Config {
+      let query = args[1].clone();
+      let filename = args[2].clone();
 
-  Config { query, filename }
+      Config { query, filename }
+  }
 }
