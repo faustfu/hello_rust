a0c7ef9ebc3de556b9f488d5aa3a78715e98b0c1
Faust Fu <faust.fu@gmail.com>
2020-02-13 15:21:41 +0000

    Dev: use Result, Closure and Process to handle exceptions.

--- src/io2.rs
+++ src/io2.rs
@@ -1,10 +1,14 @@
 use std::env; //use std::env::args_os to process invalid unicode strings.
 use std::fs;
+use std::process;
 
 pub fn io2() {
   let args: Vec<String> = env::args().collect();
 
-  let config = Config::new(&args);
+  let config = Config::new(&args).unwrap_or_else(|err| {
+    println!("Problem parsing arguments: {}", err);
+    process::exit(1);
+});
 
   println!("Searching for {}", config.query);
   println!("In file {}", config.filename);
@@ -23,10 +27,14 @@ struct Config {
 
 // Encapsulate the initial logic of members into a method likes OOP.
 impl Config {
-  fn new(args: &[String]) -> Config {
+  fn new(args: &[String]) -> Result<Config, &'static str> {
+      if args.len() < 3 {
+          return Err("not enough arguments");
+      }
+
       let query = args[1].clone();
       let filename = args[2].clone();
 
-      Config { query, filename }
+      Ok(Config { query, filename })
   }
 }
