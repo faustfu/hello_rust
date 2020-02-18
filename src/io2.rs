0353d462673e855cbb3ea1f9cb1aa57c9393270d
Faust Fu <faust.fu@gmail.com>
2020-02-18 16:30:04 +0000

    Dev: seperate error messages from standard output.

--- src/io2.rs
+++ src/io2.rs
@@ -7,13 +7,13 @@ pub fn io2() {
   let args: Vec<String> = env::args().collect();
 
   let config = hello_world::Config::new(&args).unwrap_or_else(|err| {
-    println!("Problem parsing arguments: {}", err);
+    eprintln!("Problem parsing arguments: {}", err);
     process::exit(1);
   });
 
   // run() may return errors.
   if let Err(e) = hello_world::run(config) {
-    println!("Application error: {}", e);
+    eprintln!("Application error: {}", e);
 
     process::exit(1);
   }
