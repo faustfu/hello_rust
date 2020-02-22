f0c0eb94e5dc6dae41a5ec02a2d406c416af75a1
Faust Fu <faust.fu@gmail.com>
2020-02-22 12:12:56 +0000

    Dev: move the ownership into the spwaned thread.

--- src/th1.rs
+++ src/th1.rs
@@ -1,16 +1,23 @@
 // 1. Rust uses system thread by default.
 // 2. The new thread will be stopped when the main thread ends.
 // 3. thread::sleep will stop its executio and allow a different thread to run.
+// 4. The spawned is lefttime is uncertain. So the spawned has to keep the ownership of used variables.
 
 use std::thread;
 use std::time::Duration;
 
 pub fn th1() {
-    let handle = thread::spawn(|| { // it will spawn another thread to run the closure.
+    let handle1 = thread::spawn(|| { // it will spawn another thread to run the closure.
         for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
+            println!("hi number {} from the spawned thread 1!", i);
             thread::sleep(Duration::from_millis(1));
         }
+    });
+
+    let v = vec![1, 2, 3];
+    let handle2 = thread::spawn(move || {
+        println!("Here's a vector: {:?} in the spawned thread 2!", v);
+        thread::sleep(Duration::from_millis(1));
     });
 
     for i in 1..5 {
@@ -18,5 +25,6 @@ pub fn th1() {
         thread::sleep(Duration::from_millis(1));
     }
 
-    handle.join().unwrap(); // wait for the spawned thread be completed.
+    handle2.join().unwrap(); // wait for the spawned thread 2 be completed.
+    handle1.join().unwrap(); // wait for the spawned thread 1 be completed.
 }
