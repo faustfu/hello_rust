df4e8fb5d8b29b41f09e03ca53b48b5c46cf6db7
Faust Fu <faust.fu@gmail.com>
2020-02-22 11:12:40 +0000

    Dev: wait for all threads.

--- src/th1.rs
+++ src/th1.rs
@@ -1,9 +1,12 @@
 // 1. Rust uses system thread by default.
+// 2. The new thread will be stopped when the main thread ends.
+// 3. thread::sleep will stop its executio and allow a different thread to run.
+
 use std::thread;
 use std::time::Duration;
 
 pub fn th1() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| { // it will spawn another thread to run the closure.
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
@@ -14,4 +17,6 @@ pub fn th1() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap(); // wait for the spawned thread be completed.
 }
