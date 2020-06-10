53be1760cae9853bfb7bce1720d5a369592016f8
Faust Fu <faust.fu@gmail.com>
2020-06-10 12:10:46 +0800

    Dev: add custom attribute case.

--- src/flows/macro3.rs
+++ src/flows/macro3.rs
@@ -1,15 +1,26 @@
 // 1. Prodedural macros could change token stream actively.
 
 use hello_proc_macro::A;
+use hello_proc_macro::attr_with_args;
 
 pub fn run() {
   println!("macro3");
 }
 
+// case 1(use derive to implement struct:A)
 #[derive(A)]
 struct A;
+
+// case 2(use custom attribute to replace implementation of fn:foo)
+#[attr_with_args("Hello, Rust!")]
+fn foo() {}
 
 #[test]
 fn test_derive_a() {
   assert_eq!("hello from impl A".to_string(), A.a());
+}
+
+#[test]
+fn test_foo() {
+  assert_eq!(foo(), "Hello, Rust!");
 }
