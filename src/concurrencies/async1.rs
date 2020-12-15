26553dcf916ff11acb8188e9ecc8c203ee013208
Faust Fu <faust.fu@gmail.com>
2020-12-16 00:53:23 +0800

    Dev: tweet async case.

--- src/concurrencies/async1.rs
+++ src/concurrencies/async1.rs
@@ -1,7 +1,9 @@
-// 1. Use crate:futures to use async.
-// 2. Async block has to be executed, ex: executor, await.
+// 1. Use "async" before a block will create an instance of futures.
+// 2. Use "async" before a function/closure will create a generator of futures.
+// 3. A future instance has to be executed by executor or "await".
+// 4. A future instance can only be executed one time.
 
-use futures::{self, executor};
+use futures::{join, executor};
 
 async fn my_async_1_1() {
     println!("Hello 1");
@@ -12,11 +14,14 @@ async fn my_async_1_2() {
 }
 
 async fn my_async_1() {
-    my_async_1_1().await;
-    my_async_1_2().await;
+    let f1 = my_async_1_1();
+    let f2 = my_async_1_2();
+
+    join!(f1, f2);
 }
 
 pub fn run() {
   executor::block_on(my_async_1());
+  
   println!("async1");
 }
